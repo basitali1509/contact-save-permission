d867c462f59b5865ba63b1df3cbf3eed493463bf
Basit Ali <basitali71019@gmail.com>
2023-09-20 19:03:21 +0500

    Asking permission everytime app opens bug fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:contacts_service/contacts_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,31 +27,41 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool hasExecuted = false;
   @override
   void initState() {
     super.initState();
-    saveAndRequestContactPermission();
+    checkAndExecuteContactPermission();
+  }
+
+  Future<void> checkAndExecuteContactPermission() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final bool hasExecutedBefore =
+        prefs.getBool('hasExecutedContactPermission') ?? false;
+
+    if (!hasExecutedBefore) {
+      await saveAndRequestContactPermission();
+      prefs.setBool('hasExecutedContactPermission', true);
+    }
   }
 
   Future<void> saveAndRequestContactPermission() async {
-      final status = await Permission.contacts.request();
-      if (status.isGranted) {
-        final contact = Contact(
-          givenName: 'OctDaily',
-          phones: [Item(label: 'mobile', value: '123-456-7890')],
-        );
-        await ContactsService.addContact(contact);
-        ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(
-                content: Text('OctDaily phone number saved in your Contacts successfully'),
-              duration: Duration(seconds: 3),
-              backgroundColor: Colors.blue,
-
-            ));
-      }
-      else if (status.isDenied) {
-       openAppSettings();
-      }
+    final status = await Permission.contacts.request();
+    if (status.isGranted) {
+      final contact = Contact(
+        givenName: 'OctDaily',
+        phones: [Item(label: 'mobile', value: '123-456-7890')],
+      );
+      await ContactsService.addContact(contact);
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content:
+            Text('OctDaily phone number saved in your Contacts successfully'),
+        duration: Duration(seconds: 3),
+        backgroundColor: Colors.blue,
+      ));
+    } else if (status.isDenied) {
+      openAppSettings();
+    }
   }
 
   @override
